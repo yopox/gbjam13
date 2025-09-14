c923bf4fe794a8f8fc42b4e4651b312c2395d596
yopox <dev@yopox.fr>
2025-09-14 11:41:15 +0200

    misc: simplify main scene

--- scenes/main.gd
+++ scenes/main.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 	Signals.palette_changed.connect(palette_changed)
 	Signals.change_scene.connect(change_scene)
 	change_scene(scene)
-	palette_changed(Palettes.GRAY)
+	palette_changed(Palettes.PASTEL)
 
 
 func get_scene(s: Util.Scenes) -> Resource:
