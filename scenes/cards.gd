071858261bba9b1d37f8dc7fc08d6481ef32f6b5
yopox <dev@yopox.fr>
2025-09-20 23:39:15 +0200

    cards: fix outline cards

--- scenes/cards.gd
+++ scenes/cards.gd
@@ -18,7 +18,7 @@ func _ready() -> void:
 
 func draft() -> void:
 	var cards = [card_1, card_2, card_3, card_4]
-	
+
 	if Progress.stage == 0:
 		cards[0].power = Power.pick_random_unlucky_only([])
 		cards[1].power = Power.pick_random_unlucky_only([cards[0].power])
@@ -30,7 +30,7 @@ func draft() -> void:
 		cards[2].power = Power.pick_random([cards[0].power, cards[1].power])
 		cards[3].power = Power.pick_random_unlucky([cards[0].power, cards[1].power, cards[2].power])
 
-	if Progress.stage > 0 and Progress.has(Power.ID.CLUBS_9):
+	if Progress.stage > 0 and not Progress.has(Power.ID.CLUBS_9):
 		cards[0].outline = Progress.last_killed * 4 < Progress.last_total
 		cards[3].outline = Progress.last_killed * 1.5 < Progress.last_total
 	else:
@@ -45,7 +45,7 @@ func _process(_delta: float) -> void:
 	if Util.block_input:
 		update()
 		return
-	
+
 	if Input.is_action_just_pressed("right"):
 		selected = posmod(selected + 1, 4)
 	elif Input.is_action_just_pressed("left"):
@@ -57,10 +57,10 @@ func _process(_delta: float) -> void:
 		if cards[selected].outline: return
 		Progress.add_powerup(cards[selected].power)
 		Progress.stage += 1
-		
+
 		# Heal player
 		Progress.hull = min(Progress.hull + Progress.max_hull * Values.HEAL_AFTER_STAGE_RATIO, Progress.max_hull)
-		
+
 		Signals.change_scene.emit(Util.Scenes.SPACE)
 
 
@@ -76,7 +76,7 @@ func update():
 	else:
 		power_name.text = "LOCKED"
 		@warning_ignore("integer_division")
-		var p: int = Progress.last_killed * 100 / Progress.last_total
+		var p: int = Progress.last_killed * 100 / max(1, Progress.last_total)
 		if selected == 0:
 			description.text = "Defeat 25% of all enemies\nto unlock. (" + str(p) + "%)"
 		else:
