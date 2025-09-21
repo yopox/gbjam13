3867389643c7d224c9553f1c4176bf2673703396
yopox <dev@yopox.fr>
2025-09-21 14:27:06 +0200

    bgm: add files

---
+++ scripts/music.gd
@@ -0,0 +1,11 @@
+extends Node
+
+const BOSS_THEME = preload("uid://bgv8pk5hluxt3")
+const LUCKY = preload("uid://ihsx0r6cuoh7")
+const STAGE_1 = preload("uid://dwai7fyicn1xi")
+const STAGE_2 = preload("uid://ct5vby7hiqb71")
+const STAGE_3 = preload("uid://e4akx84hv6mx")
+
+
+@onready var bgm: AudioStreamPlayer = $bgm
+@onready var bgm_2: AudioStreamPlayer = $bgm2
